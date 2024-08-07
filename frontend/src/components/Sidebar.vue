ce609dc902f6d6dba7477eb4e5492dbcab0cf2a8
Atsushi-Endo <ena-choco@live.jp>
2024-08-08 00:34:50 +0900

    CSS調整、icon追加

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="sidebar">
     <div class="sidebar-wrap">
-      <div class="text-subtitle-1">選挙得票数可視化サイト</div>
+      <div class="sidebar-title">
+        <span class="mdi mdi-chart-line-variant"></span>
+        <div class="text-subtitle-1">選挙得票数可視化サイト</div>
+      </div>
 
-      <v-expansion-panels v-model="panel" multiple>
+      <v-expansion-panels v-model="panel" multiple accordion>
         <v-expansion-panel>
           <v-expansion-panel-title>
+            <span class="mdi mdi-vote"></span>
             <label class="text-subtitle-2" for="">選挙選択</label>
           </v-expansion-panel-title>
           <v-expansion-panel-text>
             <v-select
-              density="comfortable"
+              hide-details
+              density="compact"
               label="選挙"
               :items="['東京都知事選']"
               variant="outlined"
@@ -20,11 +25,13 @@
 
         <v-expansion-panel>
           <v-expansion-panel-title>
+            <span class="mdi mdi-map-marker-outline"></span>
             <label class="text-subtitle-2" for="">エリアを選択</label>
           </v-expansion-panel-title>
           <v-expansion-panel-text>
             <v-select
-              density="comfortable"
+              hide-details
+              density="compact"
               label="市区町村"
               :items="['千代田区', '港区']"
               variant="outlined"
@@ -34,6 +41,7 @@
 
         <v-expansion-panel>
           <v-expansion-panel-title>
+            <span class="mdi mdi-map-legend"></span>
             <label class="text-subtitle-2" for="">ヒートマップ</label>
           </v-expansion-panel-title>
           <v-expansion-panel-text>
@@ -73,11 +81,34 @@ export default {
   border-radius: 4px;
 }
 
+.sidebar-title {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 8px;
+}
+
 .text-subtitle-1 {
-  padding: 16px 12px;
+  padding: 16px 0;
 }
 
 .sidebar-select {
   padding: 16px 8px 0 8px;
 }
+
+.v-expansion-panel {
+  border-radius: 0;
+  margin: 0; /* パネル間のマージンをリセット */
+  border-bottom: 1px solid #ddd; /* パネル間のボーダーを追加 */
+}
+
+.v-expansion-panel__shadow {
+  box-shadow: none !important;
+}
+
+.v-expansion-panel-title span{
+  font-size: 24px;
+  margin-right: 16px;
+}
+
 </style>
